a33f905b4991b13c600cf98345b192010e135206
samnangcattor <kcattor@gmail.com>
2017-05-15 11:16:36 +0700

    add_pagination_user_list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
   def index
-    render json: User.all
+    users = User.all
+    user_total = users.count
+    page = params[:page]
+    users = users.page(page).per Settings.users.per_page
+    last_page = user_total / Settings.users.per_page
+    response = {
+      users: users,
+      user_total: user_total,
+      current_page: page.to_i,
+      last_page: last_page
+    }
+    render json: response
   end
 end
